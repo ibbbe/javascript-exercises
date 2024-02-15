2131eb62223f910e29276801f5b83315a6d427a3
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-02-15 19:25:50 +0100

    Add solution to removeFromArray exercise test 1
    Created two parameters:
    array to store given array, and ...args to store
    unknown number of arguments.
    Looped by for...of loop through each args argument,
    and each array element searching equal ones.
    When found equals removed it with the .splice method.
    
    Changes to be committed:
    	modified:   04_removeFromArray/removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+    
+    for (arg of args) {
+        for (arr of array) {
+            if (arg === arr) {
+                array.splice(array.indexOf(arr), 1);
+            } continue;
+        }
+    } return array;
 };
 
 // Do not edit below this line
