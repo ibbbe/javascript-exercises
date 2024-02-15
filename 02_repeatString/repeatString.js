cb5ecb5600edc974a64eca4c28a6b8dbaea2354d
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-02-15 15:24:08 +0100

    Add solution to 02 and 03 exercise
    Finished two exercises. Removed skip from test.skip
    to run all tests.
    
    Changes to be committed:
    
    modified:   02_repeatString/repeatString.js
    modified:   02_repeatString/repeatString.spec.js
    modified:   03_reverseString/reverseString.js
    modified:   03_reverseString/reverseString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+    if (num < 0) return 'ERROR';
+    let result = "";
+    while (num > 0) {
+        result += word;
+        num--;
+        console.log(result);
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let length = word.length -1;
+    let half = Math.ceil(length / 2);
+    const array = word.split('');
 
+    for (let i = 0; i <= half; i++) {
+        let buffer = array[i];
+        array[i] = array[length -i];
+        array[length - i] = buffer;
+    }
+    
+    let dorW = array.join("");
+    return dorW;
 };
 
 // Do not edit below this line
