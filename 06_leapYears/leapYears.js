49c5f65931a0f029d56167b7032dbb56cd263535
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-02-16 14:38:53 +0100

    Add solution to 07 Temperature Conversion
    Write two functions with formulas for conversion.
    Rounded the result to one decimal by Math round
    result*10, and divide it by 10 after.
    
      Changes to be committed:
     	modified:   06_leapYears/leapYears.js
     	modified:   07_tempConversion/tempConversion.js
     	modified:   07_tempConversion/tempConversion.spec.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,9 @@
 const leapYears = function (year) {
-    if (year % 400 == 0) {
+    if (year % 400 === 0) {
         return true;
-    } else if (year % 100 == 0) {
+    } else if (year % 100 === 0) {
         return false;
-    } else if (year % 4 == 0) {
+    } else if (year % 4 === 0) {
         return true;
     } return false;
 };
