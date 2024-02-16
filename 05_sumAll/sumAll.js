4b43e2c57519736be49e05bcaab63e67d4fdb5f0
ibbbe <130594784+ibbbe@users.noreply.github.com>
2024-02-16 12:42:49 +0100

    solve the 05 sumAll exercise
    Checked type of arguments if they are numbers.
    Changed positions of arguments if arg2 is smaller
    than arg1 to easily sum all the numbers in range.
    
      Changes to be committed:
        modified:   05_sumAll/sumAll.js
    	modified:   05_sumAll/sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    let sum = 0;
+    if (typeof (num1) !== "number" || typeof (num2) !== "number" || num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } else {
+        if (num2 < num1) {
+            let buffer = num1;
+            num1 = num2;
+            num2 = buffer;
+        } while (num1 <= num2) {
+            sum += num1;
+            num1++;
+        } return sum;
+    }
 };
 
 // Do not edit below this line
